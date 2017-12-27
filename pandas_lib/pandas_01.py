7fbcc1885fffbcd84aed0b9165ecba07226814f3
Shubo Zhang <shubozhang@outlook.com>
2017-12-26 23:51:26 -0500

    show plot

--- pandas_lib/pandas_01.py
+++ pandas_lib/pandas_01.py
@@ -2,22 +2,21 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-s = pd.Series([1,3,5,np.nan,6,8])
+s = pd.Series([1, 3, 5, np.nan, 6, 8])
 print(s)
 
 dates = pd.date_range('20130101', periods=6)
 print(dates)
 
-df = pd.DataFrame(np.random.randn(6,4), index=dates, columns=list('ABCD'))
+df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
 print(df)
 
-df2 = pd.DataFrame({ 'A' : 1.,
-                    'B' : pd.Timestamp('20130102'),
-                    'C' : pd.Series(1,index=list(range(4)),dtype='float32'),
-                    'D' : np.array([3] * 4,dtype='int32'),
-                    'E' : pd.Categorical(["test","train","test","train"]),
-                    'F' : 'foo' })
+df2 = pd.DataFrame({'A': 1.,
+                    'B': pd.Timestamp('20130102'),
+                    'C': pd.Series(1, index=list(range(4)), dtype='float32'),
+                    'D': np.array([3] * 4, dtype='int32'),
+                    'E': pd.Categorical(["test", "train", "test", "train"]),
+                    'F': 'foo'})
 print(df2)
 print(df2.dtypes)
 
@@ -25,10 +24,12 @@ print(df.head())
 
 print(df.tail(3))
 
-
 ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
 ts = ts.cumsum()
 ts.plot()
 df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index, columns=['A', 'B', 'C', 'D'])
 df = df.cumsum()
-plt.figure(); df.plot(); plt.legend(loc='best')
+plt.figure();
+df.plot();
+plt.legend(loc='best')
+plt.show()
